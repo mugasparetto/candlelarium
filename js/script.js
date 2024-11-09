f212d791992b479dc5ff792bcceb1149df78f541
mugasparetto <mu.gasparetto@hotmail.com>
2024-11-09 01:35:58 +0000

    fixed android alignment

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    document.querySelector("body").style.fontSize = "2.4vw";
-}
+    document.querySelector('body').style.fontSize = '2.4vw';
+  }
+
+  if (/android/i.test(userAgent)) {
+    document.querySelector('main').style.left = "54.5%";
+  }
+
 });
