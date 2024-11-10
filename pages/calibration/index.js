12038245f936a4fe89ca7a27e14d5a253b209fbb
mugasparetto <mu.gasparetto@hotmail.com>
2024-11-10 12:35:57 +0000

    fixed removing styles

--- pages/calibration/index.js
+++ pages/calibration/index.js
@@ -1,8 +1,8 @@
 // pages/home.js
 export function init() {
   // cleanin up home in here to prevent bugs
-  document.querySelector('main').style.left = "50%";
-  document.querySelector('body').style.fontSize = '1rem';
+  document.querySelector('main').removeAttribute("style");
+  document.querySelector('body').removeAttribute("style");
 }
 
 export function cleanup() {
