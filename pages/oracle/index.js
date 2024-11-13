f838f4674013ae7e441d760a657a49864935d0ce
mugasparetto <mu.gasparetto@hotmail.com>
2024-11-13 07:58:51 +0000

    adjusts after silvana

--- pages/oracle/index.js
+++ pages/oracle/index.js
@@ -2,6 +2,21 @@
 export function init() {
   spa.handleLinks();
   console.log(domain);
+  document.addEventListener('signal', handleSignal);
+  document.addEventListener('speechstop', handleStop);
 }
 
 export function cleanup() {}
+
+function handleSignal(event) {
+  const dBV = dB(event.detail.volume);
+  console.log(dBV);
+
+  if (dBV >= BLOW_THRESHOLD) {
+    document.querySelector('#debug').textContent = 'blow';
+  }
+}
+
+function handleStop(event) {
+  document.querySelector('#debug').textContent = 'not blow';
+}
