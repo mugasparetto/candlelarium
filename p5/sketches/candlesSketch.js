f0d00ec711ed4ef564648b8c5027afaedd9ab05b
mugasparetto <mu.gasparetto@hotmail.com>
2024-11-17 17:17:25 +0000

    added initial fade to candles sketch

--- p5/sketches/candlesSketch.js
+++ p5/sketches/candlesSketch.js
@@ -3,6 +3,9 @@ export class CandlesSketch {
     this.p = p;
     this.waves = [];
     this.ascents = {};
+    this.shouldInitialFade = true;
+    this.initialFade = 255;
+    this.initialDelay = 10;
 
     p.setup = () => this.setup();
     p.draw = () => this.draw();
@@ -28,46 +31,22 @@ export class CandlesSketch {
   draw() {
     this.p.background(0);
 
-    this.p.fill(0);
-    this.p.text(
-      '🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️',
-      this.p.width / 2,
-      5.5 * CELL_SIZE + 0.45 * this.ascents.candle
-    );
-    this.p.text(
-      '🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️',
-      this.p.width / 2,
-      7.5 * CELL_SIZE + 0.45 * this.ascents.candle
-    );
-    this.p.text(
-      '🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️',
-      this.p.width / 2,
-      9.5 * CELL_SIZE + 0.45 * this.ascents.candle
-    );
-    this.p.text(
-      '🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️',
-      this.p.width / 2,
-      11.5 * CELL_SIZE + 0.45 * this.ascents.candle
-    );
-    this.p.text(
-      '🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️',
-      this.p.width / 2,
-      13.5 * CELL_SIZE + 0.45 * this.ascents.candle
-    );
-    this.p.text(
-      '🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️',
-      this.p.width / 2,
-      15.5 * CELL_SIZE + 0.45 * this.ascents.candle
-    );
-    this.p.text(
-      '🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️',
-      this.p.width / 2,
-      17.5 * CELL_SIZE + 0.45 * this.ascents.candle
-    );
-    this.p.text(
-      '🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️  🕯️',
-      this.p.width / 2,
-      19.5 * CELL_SIZE + 0.45 * this.ascents.candle
-    );
+    if (this.p.frameCount > this.initialDelay) {
+      this.p.fill(0);
+    }
+
+    if (this.shouldInitialFade && this.p.frameCount > this.initialDelay) {
+      this.p.fill(0, this.initialFade);
+      this.p.rect(
+        this.p.width / 2,
+        this.p.height / 2,
+        this.p.width,
+        this.p.height
+      );
+      this.initialFade -= 30;
+      if (this.initialFade < 0) {
+        this.shouldInitialFade = false;
+      }
+    }
   }
 }
