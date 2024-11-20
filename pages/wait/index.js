237ebb501e28bdce532d248b998e29dcf5fd4969
mugasparetto <mu.gasparetto@hotmail.com>
2024-11-20 11:36:11 +0000

    feat: wait screen

--- pages/wait/index.js
+++ pages/wait/index.js
@@ -1,9 +1,10 @@
 export function init() {
   const oracleReadingTime = localStorage.getItem('ORACLE_READING_TIME');
-  console.log(oracleReadingTime);
   const diff = dayjs().diff(oracleReadingTime, 'm', true);
   const hours = Math.ceil(24 - diff / 60);
-  console.log(`less than ${hours} ${hours === 1 ? 'hour' : 'hours'}`);
+  document.querySelector('#wait-time').textContent = `allow it about ${hours} ${
+    hours === 1 ? 'hour' : 'hours'
+  } to gather its strength`;
 }
 
 export function cleanup() {}
