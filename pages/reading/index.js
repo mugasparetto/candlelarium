768f2cd221a0e1ba65fc0c16d2a74030bc950850
mugasparetto <mu.gasparetto@hotmail.com>
2024-11-20 04:50:06 +0000

    reading layout

--- pages/reading/index.js
+++ pages/reading/index.js
@@ -1,4 +1,10 @@
 export async function init() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/android/i.test(userAgent)) {
+    document.querySelector('.home-art').style.left = '52.5%';
+  }
+
   // MOCKED DATA:
   // domain = 'physical';
   // result = [
@@ -20,13 +26,35 @@ export async function init() {
         body: JSON.stringify({ domain, result }),
       }
     );
-    const data = await response.json();
-    document.querySelector('#debug').textContent = data.reading;
-    console.log(data.reading);
+
+    if (response.status === 200) {
+      const data = await response.json();
+
+      showReading(data.reading);
+    } else {
+      showReading(
+        'the oracle is still looking for the proper answer. come back later.'
+      );
+    }
   } catch (error) {
-    document.querySelector('#debug').textContent = error;
+    showReading(
+      'the oracle is still looking for the proper answer. come back later.'
+    );
     console.log(error);
   }
 }
 
 export function cleanup() {}
+
+function showReading(content) {
+  const div = document.createElement('div');
+  div.classList.add('content');
+  div.innerHTML = `<div class="reading">
+  <span class="corner">✨</span>
+  <span class="corner">✨</span>
+  <span class="corner">✨</span>
+  <span class="corner">✨</span>
+  <small>${content}</small>
+  </div>`;
+  document.querySelector('#app').appendChild(div);
+}
