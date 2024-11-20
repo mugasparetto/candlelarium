2ce2e95d1f70bf761b08d0b5ff8fdf3cc3937063
mugasparetto <mu.gasparetto@hotmail.com>
2024-11-20 07:40:12 +0000

    calculating time diff

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,11 +1,21 @@
 // pages/home.js
 export function init() {
-  spa.handleLinks();
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
   if (/android/i.test(userAgent)) {
     document.querySelector('.home-art').style.left = '52.5%';
   }
+
+  document.querySelector('a').addEventListener('click', goToNextScreen);
 }
 
-export function cleanup() {}
+export function cleanup() {
+  document.querySelector('a').removeEventListener('click', goToNextScreen);
+}
+
+function goToNextScreen(event) {
+  event.preventDefault();
+  const oracleReadingTime = localStorage.getItem('ORACLE_READING_TIME');
+
+  console.log(dayjs().diff(oracleReadingTime, 'm', true));
+}
